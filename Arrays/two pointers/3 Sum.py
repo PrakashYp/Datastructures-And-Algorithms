7f3b8a3f04d2e0e2e55f0ad34233f99e172fbf08
Prakash Yp <ypprakash835@gmail.com>
2026-04-12 20:41:59 +0530

    Started 3 Sum problem for better runtime

--- Arrays/two pointers/3 Sum.py
+++ Arrays/two pointers/3 Sum.py
@@ -1,24 +1,17 @@
-# def findTriplets(nums):
-#     i=0
-#     j=1
-#     ans_list = set()
+def findTriplets(nums):
+    i=0
+    j=1
+    ans_list = set()
 
-#     for i in range(len(nums)):
-#         for j in range(i+1,len(nums)):
-#             for k in range(j+1,len(nums)):
-#                 sum = nums[i]+nums[j]+nums[k]
-#                 if sum == 0:
-                    
-#                     ans_list.add(tuple(sorted([nums[i],nums[j],nums[k]])))
+    for i in range(len(nums)):
+        pass
 
 
-#     return list(ans_list)
+    return list(ans_list)
 
 
-# arr = [-1,0,1,2,-1,-4]
-# print(findTriplets(arr))
+arr = [-1,0,1,2,-1,-4]
+print(findTriplets(arr))
 
 
-
-print(type(True))
 
