f371adc95aa99f3debe39159b6622613d49cebf5
Prakash Yp <ypprakash835@gmail.com>
2026-05-07 13:11:52 +0530

    Solved Max Consecutive ones I

---
+++ Arrays/sliding window/Max Consecutive Ones.py
@@ -0,0 +1,20 @@
+def findMaxConsecutiveOnes(nums):
+    """
+    :type nums: List[int]
+    :rtype: int
+    """
+    count = 0
+    max_count = 0
+    for i in range(len(nums)):
+        if nums[i] == 1:
+            count += 1 
+        
+        else:
+            count = 0
+
+        max_count = max(max_count,count)
+    
+    return max_count
+nums =  [1,0,1,1,1,1,0,1]
+print(findMaxConsecutiveOnes(nums))
+
--- Arrays/two pointers/Sort_colors.py
+++ Arrays/two pointers/Sort_colors.py
@@ -13,16 +13,52 @@ def sortColors(nums):
             swap = nums[red_pointer]          
             nums[red_pointer] =  nums[current]
             nums[current] = swap
-            red_pointer += 1
+            red_pointer += 1    
             current+=1
         elif nums[current] == 2:
             swap = nums[blue_pointer]          
             nums[blue_pointer] =  nums[current]
             nums[current] = swap
             blue_pointer-=1
-        
-        current+=1
+            continue
+        current += 1
     return nums
 
 nums = [2,0,1]
 print(sortColors(nums))
+
+# def sort_colors(nums):
+#     red_count = 0
+#     white_count = 0
+#     blue_count = 0
+#     for num in nums:
+#         if num == 0:
+#             red_count+=1
+#         if num == 1:
+#             white_count+=1
+#         if num == 2:
+#             blue_count+=1
+
+#     print(red_count,white_count,blue_count)
+    
+#     for i in range(len(nums)):
+#         if red_count>0:
+#             nums[i] = 0
+#             red_count-=1
+       
+#         elif white_count > 0:
+#             nums[i] = 1
+#             white_count -= 1
+            
+#         elif blue_count > 0:
+#             nums[i] = 2
+#             blue_count -= 1
+
+
+#     return nums
+
+
+# nums = [2,0,2,1,1,0]
+# print(sort_colors(nums))
+
+
