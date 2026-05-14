7a740be079cba7e80aaee75ad5765f7e8958103a
Prakash Yp <ypprakash835@gmail.com>
2026-05-14 16:52:17 +0530

    Solved Best time to buy and sell stock problem

---
+++ Arrays/Kadane's algorithm/Best Time to Buy and Sell Stock.py
@@ -0,0 +1,26 @@
+
+
+def maxProfit(prices):
+    """
+    :type prices: List[int]
+    :rtype: int
+    """
+    best_day = prices[0]
+    max_profit = 0
+
+
+    for i in range(len(prices)):
+        best_day = min(best_day,prices[i])
+        max_profit = max(max_profit,prices[i] - best_day)
+        print(best_day    , max_profit)
+
+
+
+    return max_profit
+
+
+
+
+prices = [7,1,5,3,6,4]
+
+print(maxProfit(prices))
