624c3edebf7a2d224832ff4ee69ddf1bc68efca3
Prakash Yp <ypprakash835@gmail.com>
2026-05-20 23:01:13 +0530

    Solved Maximum Product Subarray problem

--- Arrays/Kadane's algorithm/Maximum Product Subarray.py
+++ Arrays/Kadane's algorithm/Maximum Product Subarray.py
@@ -3,4 +3,23 @@ def maxProduct(nums):
     :type nums: List[int]
     :rtype: int
     """
-        
+    current_min = nums[0]
+    current_max = nums[0]
+    result = nums[0]
+    for i in range(1,len(nums)):
+        temp_max = max(nums[i],nums[i]*current_max,nums[i]*current_min)
+        temp_min = min(nums[i],nums[i]*current_max,nums[i]*current_min)
+        current_max = temp_max
+        current_min = temp_min
+
+        result = max(result,current_max)
+
+    return result
+
+
+
+print(maxProduct([2, 3, -2, 4]))
+
+
+
+
